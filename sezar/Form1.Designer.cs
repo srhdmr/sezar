0bf654e822dfeed96ae8c422311773597042f736
Serhat <serhattdemirr2005@gmail.com>
2024-11-05 19:27:38 +0300

    Proje dosyası ekle.

---
+++ sezar/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace sezar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtşifrelenecek = new System.Windows.Forms.TextBox();
+            this.txtveri = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(397, 52);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ŞİFRELENECEK VERİ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(258, 52);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ŞİFRELİ VERİ";
+            // 
+            // txtşifrelenecek
+            // 
+            this.txtşifrelenecek.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtşifrelenecek.Location = new System.Drawing.Point(427, 40);
+            this.txtşifrelenecek.Name = "txtşifrelenecek";
+            this.txtşifrelenecek.Size = new System.Drawing.Size(361, 45);
+            this.txtşifrelenecek.TabIndex = 3;
+            // 
+            // txtveri
+            // 
+            this.txtveri.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtveri.Location = new System.Drawing.Point(299, 140);
+            this.txtveri.Name = "txtveri";
+            this.txtveri.Size = new System.Drawing.Size(489, 43);
+            this.txtveri.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(464, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 89);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "GÖSTER";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtveri);
+            this.Controls.Add(this.txtşifrelenecek);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtşifrelenecek;
+        private System.Windows.Forms.TextBox txtveri;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ sezar/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sezar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String veri = txtşifrelenecek.Text;
+            String şifreli_veri = "";
+            for (int i = 0; i < veri.Length; i++)
+            {
+                if (veri[i].ToString() == "A" || veri[i].ToString() == "a") { şifreli_veri += "Ç"; }
+                else if (veri[i].ToString() == "B" || veri[i].ToString() == "b") { şifreli_veri += "D"; }
+                else if (veri[i].ToString() == "C" || veri[i].ToString() == "c") { şifreli_veri += "E"; }
+                else if (veri[i].ToString() == "Ç" || veri[i].ToString() == "ç") { şifreli_veri += "F"; }
+                else if (veri[i].ToString() == "D" || veri[i].ToString() == "d") { şifreli_veri += "G"; }
+                else if (veri[i].ToString() == "E" || veri[i].ToString() == "e") { şifreli_veri += "Ğ"; }
+                else if (veri[i].ToString() == "F" || veri[i].ToString() == "f") { şifreli_veri += "H"; }
+                else if (veri[i].ToString() == "G" || veri[i].ToString() == "g") { şifreli_veri += "I"; }
+                else if (veri[i].ToString() == "Ğ" || veri[i].ToString() == "ğ") { şifreli_veri += "İ"; }
+                else if (veri[i].ToString() == "H" || veri[i].ToString() == "h") { şifreli_veri += "J"; }
+                else if (veri[i].ToString() == "I" || veri[i].ToString() == "ı") { şifreli_veri += "K"; }
+                else if (veri[i].ToString() == "İ" || veri[i].ToString() == "i") { şifreli_veri += "L"; }
+                else if (veri[i].ToString() == "J" || veri[i].ToString() == "j") { şifreli_veri += "M"; }
+                else if (veri[i].ToString() == "K" || veri[i].ToString() == "k") { şifreli_veri += "N"; }
+                else if (veri[i].ToString() == "L" || veri[i].ToString() == "l") { şifreli_veri += "O"; }
+                else if (veri[i].ToString() == "M" || veri[i].ToString() == "m") { şifreli_veri += "Ö"; }
+                else if (veri[i].ToString() == "N" || veri[i].ToString() == "n") { şifreli_veri += "P"; }
+                else if (veri[i].ToString() == "O" || veri[i].ToString() == "o") { şifreli_veri += "R"; }
+                else if (veri[i].ToString() == "Ö" || veri[i].ToString() == "ö") { şifreli_veri += "S"; }
+                else if (veri[i].ToString() == "P" || veri[i].ToString() == "p") { şifreli_veri += "Ş"; }
+                else if (veri[i].ToString() == "R" || veri[i].ToString() == "r") { şifreli_veri += "T"; }
+                else if (veri[i].ToString() == "S" || veri[i].ToString() == "s") { şifreli_veri += "U"; }
+                else if (veri[i].ToString() == "Ş" || veri[i].ToString() == "ş") { şifreli_veri += "Ü"; }
+                else if (veri[i].ToString() == "T" || veri[i].ToString() == "t") { şifreli_veri += "V"; }
+                else if (veri[i].ToString() == "U" || veri[i].ToString() == "u") { şifreli_veri += "Y"; }
+                else if (veri[i].ToString() == "Ü" || veri[i].ToString() == "ü") { şifreli_veri += "Z"; }
+                else if (veri[i].ToString() == "V" || veri[i].ToString() == "v") { şifreli_veri += "A"; }
+                else if (veri[i].ToString() == "Y" || veri[i].ToString() == "y") { şifreli_veri += "B"; }
+                else if (veri[i].ToString() == "Z" || veri[i].ToString() == "z") { şifreli_veri += "C"; }
+                else
+                {
+                    şifreli_veri = veri[i].ToString();
+                }
+
+            }
+            txtveri.Text = şifreli_veri;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
